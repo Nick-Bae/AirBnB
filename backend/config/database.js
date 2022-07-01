61d575fcaa51ea1af79b995453cfbf1e19ee6945
namju <namjubae@gmail.com>
2022-07-01 13:57:22 -0500

    heroku-revision

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,7 @@ module.exports = {
     typeValidation: true
   },
   production: {
-    use_env_variable: 'postgres://junxqaylgzrnda:3da653719420b4a5af30a274fec07eba8fe2e926be0f6f72e3137dc4bdc83a04@ec2-44-206-89-185.compute-1.amazonaws.com:5432/d57hcbg02h67bk',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
