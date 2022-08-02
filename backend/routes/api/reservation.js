6c61a2414aef304bd3d45baaaa27233e26cd13d0
namju <namjubae@gmail.com>
2022-08-02 18:41:29 -0500

    get all of the current User's Bookings ALL DONE

--- backend/routes/api/reservation.js
+++ backend/routes/api/reservation.js
@@ -4,52 +4,51 @@ const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth')
 const { User, Spot, Booking, Review, sequelize, Image } = require('../../db/models');
 
 const router = express.Router();
-const {Op}=require("sequelize")
+const { Op } = require("sequelize")
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const validateReservation =[
+const validateReservation = [
     check('spotId')
-        .exists({checkFalsy: true})
+        .exists({ checkFalsy: true })
         .withMessage('')
 ]
 
 // ============ Get all of the Current User's Bookings =================
-router.get('/current', async(req, res)=>{
-    const {user}=req
+router.get('/current', async (req, res) => {
+    const { user } = req
     // res.json(user.id)
     const reservations = await Booking.findAll({
-        where: {userId: user.id},
-        // attributes: ['id','spotId'],
-        include: [{model:Spot, attributes: {exclude:['description','createdAt','updatedAt']}},
-        // {
-        //                 model: Image,
-        //                 where: {
-        //                     previewImage: true
-        //                 },
-        //                 as: "previewImage"
-        //             }
-    ]
+        where: { userId: user.id },
+        include: {
+            model: Spot,
+            include: {
+                model: Image,
+                where: {
+                    previewImage: true
+                },
+                as: "previewImage"
+            }
+        }
     });
-    // const image = await Image.find
-    const bookings = reservations.map(booking => place={
+    const image = await Image.find
+    const bookings = reservations.map(booking => place = {
         id: booking.id, spotId: booking.spotId,
-        Spot: {address: booking.Spot.address, city: booking.Spot.city,
+        Spot: {
+            address: booking.Spot.address, city: booking.Spot.city,
             state: booking.Spot.state, country: booking.Spot.country,
             lat: booking.Spot.lat, lng: booking.Spot.lng, name: booking.Spot.name,
-             price: booking.Spot.price,
-        }, 
+            price: booking.Spot.price, previewImage: booking.Spot.previewImage[0].url
+        },
         userId: booking.userId, startDate: booking.startDate, endDate: booking.endDate,
         createdAt: booking.createdAt, updatedAt: booking.updatedAt
     })
 
-
-
-    res.json({Bookings:bookings})
+    res.json({ Bookings: bookings })
     // res.json({Bookings:reservations})
 })
 
 //Get all Bookings for a Spot based on t{he Spot's id
-router.get('/spot/:spotId', async(req, res)=>{
+router.get('/spot/:spotId', async (req, res) => {
     // try {
     //     const reservations = await Reservation.findAll({
     //         where : {spotId: req.params.spotId}
@@ -64,66 +63,66 @@ router.get('/spot/:spotId', async(req, res)=>{
     //     )
     // }
     const reservations = await Booking.findAll({
-                where : {spotId: req.params.spotId}
-            })
+        where: { spotId: req.params.spotId }
+    })
 
-    if (reservations.length === 0){
-    // if (!reservations){
+    if (reservations.length === 0) {
+        // if (!reservations){
         res.status(404).json({
-                "message": "Spot couldn't be found",
-                "statusCode": 404
-              })
+            "message": "Spot couldn't be found",
+            "statusCode": 404
+        })
     } else {
         res.json(reservations)
     }
 })
 
 // Create a Booking from a Spot based on the Spot's id
-router.post('/spot/:spotId/user/:userId', async(req, res)=>{
+router.post('/spot/:spotId/user/:userId', async (req, res) => {
     // try{
-        const {spotId, checkIn,checkOut} = req.body
-        const spot = await Spot.findByPk(req.params.spotId)
-        if (spot === null) res.status(404).json({
-                "message": "Spot couldn't be found",
-                "statusCode": 404
-        })
-        const reservInSpot = await Booking.findAll({
-            where: {
-                spotId: spotId,
-                [Op.or]: [
-                    {
-                      checkIn: {
-                        [Op.between]: [checkIn, checkOut]
-                      }
-                    },
-                    {
-                      checkOut: {
-                        [Op.between]: [checkIn, checkOut]
-                      }
-                    }
-                  ]
-            }
-        })
-    
-        if (reservInSpot.length !== 0){
-            res.status(403).json(
+    const { spotId, checkIn, checkOut } = req.body
+    const spot = await Spot.findByPk(req.params.spotId)
+    if (spot === null) res.status(404).json({
+        "message": "Spot couldn't be found",
+        "statusCode": 404
+    })
+    const reservInSpot = await Booking.findAll({
+        where: {
+            spotId: spotId,
+            [Op.or]: [
                 {
-                    "message": "Sorry, this spot is already booked for the specified dates",
-                    "statusCode": 403,
-                    "errors": {
-                      "startDate": "Start date conflicts with an existing booking",
-                      "endDate": "End date conflicts with an existing booking"
+                    checkIn: {
+                        [Op.between]: [checkIn, checkOut]
                     }
-                  }
-            )
-        } else {
-            const newBooking = await Booking.create({
-                spotId: req.params.spotId,
-                userId: req.params.userId,
-                checkIn,checkOut
-            })
-            res.json(newBooking)
+                },
+                {
+                    checkOut: {
+                        [Op.between]: [checkIn, checkOut]
+                    }
+                }
+            ]
         }
+    })
+
+    if (reservInSpot.length !== 0) {
+        res.status(403).json(
+            {
+                "message": "Sorry, this spot is already booked for the specified dates",
+                "statusCode": 403,
+                "errors": {
+                    "startDate": "Start date conflicts with an existing booking",
+                    "endDate": "End date conflicts with an existing booking"
+                }
+            }
+        )
+    } else {
+        const newBooking = await Booking.create({
+            spotId: req.params.spotId,
+            userId: req.params.userId,
+            checkIn, checkOut
+        })
+        res.json(newBooking)
+    }
     // } catch (err){
     //     res.json({
     //             "message": "Spot couldn't be found",
@@ -133,8 +132,8 @@ router.post('/spot/:spotId/user/:userId', async(req, res)=>{
 })
 
 //Edit a Booking
-router.put('/:reservationId', async(req, res)=>{
-    const {checkIn,checkOut} = req.body
+router.put('/:reservationId', async (req, res) => {
+    const { checkIn, checkOut } = req.body
     const editBooking = await Booking.findByPk(req.params.reservationId)
 
     editBooking.update({
@@ -147,34 +146,34 @@ router.put('/:reservationId', async(req, res)=>{
             "statusCode": 404
         })
     } else {
-        
+
         res.json(editBooking)
     }
 })
 
 //Delete a Booking
-router.delete('/:reservationId', async(req,res)=>{
-    try{
-    const date = await Booking.findByPk(req.params.reservationId, {
-    })
-    let today = new Date().toISOString().slice(0,10)
-    if (date.checkIn < today){
-        res.json({
-            "message": "Bookings that have been started can't be deleted",
-            "statusCode": 400
+router.delete('/:reservationId', async (req, res) => {
+    try {
+        const date = await Booking.findByPk(req.params.reservationId, {
         })
-    } else {
+        let today = new Date().toISOString().slice(0, 10)
+        if (date.checkIn < today) {
+            res.json({
+                "message": "Bookings that have been started can't be deleted",
+                "statusCode": 400
+            })
+        } else {
 
-        const deleteBook = await Booking.findByPk(req.params.reservationId)
-        deleteBook.destroy()
-        res.status(200)
+            const deleteBook = await Booking.findByPk(req.params.reservationId)
+            deleteBook.destroy()
+            res.status(200)
             res.json({
                 "message": "Successfully deleted",
-                "statusCode": 200 
+                "statusCode": 200
             })
-    }
-   
-    }catch (err){
+        }
+
+    } catch (err) {
         res.status(404)
         res.json({
             "message": "Reservation couldn't be found",
