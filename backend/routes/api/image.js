eda9e7345097b50680fb68be9235c56544fb734c
namju <namjubae@gmail.com>
2022-08-03 01:04:35 -0500

    delete an Image ALL DONE

--- backend/routes/api/image.js
+++ backend/routes/api/image.js
@@ -8,12 +8,23 @@ const {Op}=require("sequelize")
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
-router.delete('/:imageId/:imageableType', async(req, res)=>{
+router.delete('/:imageId', requireAuth, async(req, res)=>{
     const {user}=req
     const deleteImage = await Image.findByPk(req.params.imageId);
-    res.json(deleteImage)
-    // user.id === imageableId
-    
+    if (deleteImage === null) {
+        res.status(404).json({
+            "message": "Image couldn't be found",
+            "statusCode": 404
+          })
+    } else if (user.id === deleteImage.userId) {
+        deleteImage.destroy();
+        res.json({
+            "message": "Successfully deleted",
+            "statusCode": 200
+          })
+    } else {
+        res.status(403).json("No permission")
+    }
 })
 
 
